bef701e2edf30d8105b584571753f6ba4406dffc
Emil Velikov <emil.l.velikov@gmail.com>
2020-02-13 08:35:41 +0000

    Drop explcit python2 from shebang
    As of last commit we're in python2+3 land.
    
    v2: Drop the TODO - tests are updated
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- src/gtimelog/main.py
+++ src/gtimelog/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 """
 A Gtk+ application for keeping track of time.
 
--- src/gtimelog/rltimelog.py
+++ src/gtimelog/rltimelog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 '''A readline interface for gtimelog.'''
 

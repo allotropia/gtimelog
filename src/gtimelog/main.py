allotropia/gtimelog

commit 11bdaaeccc7b755b4424315379f4b06cb0aed0b3

gtimelog: go through all task data via the search box
Unlike upstream, we have a segmented task list. In consists of four
components - customer:project:group:task

Currently when the user writes "test", while searching for tasks, the
text is matched only with the "group" entries. This is fairly misleading
since the user may be looking for the any of the four.

Reported-by: Eze Garcia <ezequiel@collabora.com>
Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

src/gtimelog/main.py
@@ -338,21 +338,16 @@ class MainWindow(object):
 
             depth = model.iter_depth(iter)
 
-            if depth == 2:
-                return txt.lower() in model.get_value(iter, 0).lower()
-
-            elif depth < 2:
+            if not txt.lower() in model.get_value(iter, 0).lower():
                 child = model.iter_children(iter)
 
                 while child is not None:
                     if _task_filter_filter(model, child, None):
                         return True
                     child = model.iter_next(child)
-
                 return False
 
-            else:
-                return True
+            return True
 
         task_filter.connect("changed", _task_filter_changed)
         task_filter.connect("icon-release", _task_filter_clear)

commit 331cf615868a3f1e1912cd3814440b22a480c309

gtimelog: display the full tree on match
Earlier commit started matching any of the task data, although it did
not show the full task tree, when a match was made. Namely for a given:

customer:project:group:task

If we matched the group, none of the tasks were listed. This commit
fixes that adding a blucky comment explaining the current, IMHO weird,
state of affairs.

Fixes: 11bdaae ("gtimelog: go through all task data via the search box")
Reported-by: Fadi Abou Younes <fadi.abouyounes@collabora.com>
Signed-off-by: Emil Velikov <emil.velikov@collabora.com>

src/gtimelog/main.py
@@ -336,7 +336,20 @@ class MainWindow(object):
 
             depth = model.iter_depth(iter)
 
-            if not txt.lower() in model.get_value(iter, 0).lower():
+            # Currently the first (0th) column of the data is the respective
+            # section, as separated by :, while the second (1th) column has
+            # the complete path, including the secion - foo:bar:section
+            #
+            # Seemingly the current format confuses the treeview model, since
+            # it will traverse into the children, _even_ when the parent
+            # returns false. Additionally, even as the child returns true, its
+            # parents are checked and the field is displayed _only_ iff the
+            # child (and later on parent) return true.
+            #
+            # All this is pretty weird, but using the second (1st) column gets
+            # us where we want - aka show the full tree given any section is
+            # matched.
+            if not txt.lower() in model.get_value(iter, 1).lower():
                 child = model.iter_children(iter)
 
                 while child is not None:
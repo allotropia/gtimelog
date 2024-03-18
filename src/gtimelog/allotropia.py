allotropia/gtimelog

commit f82d97ca497b71a60adb50cbbffd80819086c23b

Switch from print() to sys.stderr.write() for errors
Avoids cluttering CSV output with errors and warnings (and make those
visible in the terminal, at the time of export)

src/gtimelog/allotropia.py
@@ -57,7 +57,7 @@ class RemoteTaskList(TaskList):
                 out = open(self.filename, 'w')
                 out.write(message.response_body.data)
             except IOError as e:
-                print(e)
+                sys.stderr.write(e)
                 if self.error_callback:
                     self.error_callback()
             finally:
@@ -73,7 +73,7 @@ class RemoteTaskList(TaskList):
             self.loading_callback()
 
         if self.settings.server_cert and not os.path.exists(self.settings.server_cert):
-            print("Server certificate file not found")
+            sys.stderr.write("Server certificate file not found")
 
         message = Soup.Message.new('GET', self.url)
         soup_session.queue_message(message, self.download_finished_cb, None)

src/gtimelog/main.py
@@ -11,9 +11,9 @@ if DEBUG:
     def mark_time(what=None, _prev=[0, 0]):
         t = time.time()
         if what:
-            print("{:.3f} ({:+.3f}) {}".format(t - _prev[1], t - _prev[0], what))
+            sys.stderr.write("{:.3f} ({:+.3f}) {}".format(t - _prev[1], t - _prev[0], what))
         else:
-            print()
+            sys.stderr.write()
             _prev[1] = t
         _prev[0] = t
 else:
@@ -77,8 +77,8 @@ try:
     from gi.repository import Notify
     assert Notify.init("gtimelog")
 except ImportError:
-    print("LibNotify (with introspection) not found.")
-    print("Idle timeouts are not supported.")
+    sys.stderr.write("LibNotify (with introspection) not found.")
+    sys.stderr.write("Idle timeouts are not supported.")
 mark_time("Gtk imports done")
 
 from .allotropia import RemoteTaskList, soup_session  # noqa: E402
@@ -460,7 +460,7 @@ class MainWindow(object):
 
             self.main_window.move(x, y)
         except Exception as e:
-            print(e.message)
+            sys.stderr.write(e.message)
 
     def save_ui_state(self, filename):
         try:
@@ -480,7 +480,7 @@ class MainWindow(object):
             config.write(uistaterc)
             uistaterc.close()
         except Exception as e:
-            print(e.message)
+            sys.stderr.write(e.message)
 
     def set_up_log_view_columns(self):
         """Set up tab stops in the log view."""
@@ -588,7 +588,7 @@ class MainWindow(object):
         try:
             open(os.path.join(configdir, filename), 'w').write('%d' % number)
         except IOError as e:
-            print('Unable to record ack...: ' + e.message)
+            sys.stderr.write('Unable to record ack...: ' + e.message)
             pass
 
     def ack_weekly_reminder(self, *args):
@@ -1249,8 +1249,8 @@ class MainWindow(object):
             with open(filename, 'rb') as f:
                 togglesdict = pickle.load(f)
         except (IOError, pickle.PickleError) as e:
-            print("ERROR READING TOGGLE STATE FROM DISK")
-            print(e)
+            sys.stderr.write("ERROR READING TOGGLE STATE FROM DISK")
+            sys.stderr.write(e)
             togglesdict = {}
 
         return togglesdict
@@ -1263,8 +1263,8 @@ class MainWindow(object):
             with open(filename, 'wb') as f:
                 pickle.dump(togglesdict, f)
         except (IOError, pickle.PickleError) as e:
-            print("FAILED TO WRITE TOGGLE STATE TO DISK")
-            print(e)
+            sys.stderr.write("FAILED TO WRITE TOGGLE STATE TO DISK")
+            sys.stderr.write(e)
 
     def on_row_expander_changed(self, treeview, iter, path, expanded):
         """Someone toggled a task list expander"""
@@ -1595,7 +1595,7 @@ class MainWindow(object):
                 self.welcome_back_notification.connect(
                     'closed', self.__notification_closed_cb)
         except Exception as e:
-            print("pynotification failed: %s" % e)
+            sys.stderr.write("pynotification failed: %s" % e)
 
     def insert_old_log_entries(self, note=None, act=None, data=None):
         """
@@ -1854,7 +1854,7 @@ class SubmitWindow(object):
 
     def upload(self, data, final_date, automatic):
         if self.settings.server_cert and not os.path.exists(self.settings.server_cert):
-            print("Server certificate file '%s' not found" %
+            sys.stderr.write("Server certificate file '%s' not found" %
                   self.settings.server_cert)
 
         # PUT the file onto a nextcloud share - curl equivalent is:
@@ -1867,7 +1867,7 @@ class SubmitWindow(object):
         soup_session.queue_message(message, self.upload_finished, automatic)
 
     def error_dialog(self, e, title='Error Communicating With The Server', automatic=False):
-        print(e)
+        sys.stderr.write(e)
         if automatic:
             self.push_error_infobar(title, e)
         else:
@@ -2017,7 +2017,7 @@ class SubmitWindow(object):
                 continue
 
             if line and line != "Failed":
-                print("Couldn't understand server: %s" % line)
+                sys.stderr.write("Couldn't understand server: %s" % line)
 
     def hide(self):
         self.window.hide()

src/gtimelog/secrets.py
@@ -35,7 +35,7 @@ class Authenticator(object):
             }
         )
     except ImportError:
-        print("LibSecret not found. You will not be able to use the password keyring.")
+        sys.stderr.write("LibSecret not found. You will not be able to use the password keyring.")
         Secret = None
 
     def find_in_keyring(self, uri, callback):
@@ -71,7 +71,7 @@ class Authenticator(object):
                     self.SECRET_SCHEMA_COMPAT_NETWORK, attrs, None)
         except (GLib.Error, KeyError) as e:
             # Couldn't contact daemon, or other errors
-            print("Unable to contact keyring: {0}".format(e))
+            sys.stderr.write("Unable to contact keyring: {0}".format(e))
 
         callback(username, password)
 
@@ -93,7 +93,7 @@ class Authenticator(object):
                 "Chronophage password for GTimelog", password, None)
         except GLib.Error as e:
             # Couldn't contact daemon, or other errors
-            print("Unable to contact keyring: {0}".format(e))
+            sys.stderr.write("Unable to contact keyring: {0}".format(e))
 
     def ask_the_user(self, auth, uri, callback):
         """Pops up a username/password dialog for uri"""

src/gtimelog/timelog.py
@@ -253,7 +253,7 @@ class TimeWindow(object):
                     continue
 
                 if self.items and time <= self.items[-1][0]:
-                    print("WARNING: This entry out of order:", line)
+                    sys.stderr.write("WARNING: This entry out of order: " + line)
                 self.items.append((time, entry))
 
         # The entries really should be already sorted in the file
@@ -817,7 +817,7 @@ class TaskList(object):
                     if line and not line.startswith("#"):
                         self.items.add(line)
         except IOError as e:
-            print(e.message)
+            sys.stderr.write(e.message)
             pass  # the file's not there, so what?
 
     def reload(self):

commit fbb8902b577cad9a4b04fcc4a78dbc2c2497c70c

switch to print(file=sys.stderr) for errors
print() does automatic conversions that stderr.write() doesn't do, so
starting gtimelog with a fresh config directory failed because an
exception was passed directly to write().

(regression from commit f82d97ca497b71a60adb50cbbffd80819086c23b)

src/gtimelog/allotropia.py
@@ -57,7 +57,7 @@ class RemoteTaskList(TaskList):
                 out = open(self.filename, 'w')
                 out.write(message.response_body.data)
             except IOError as e:
-                sys.stderr.write(e)
+                print(e, file=sys.stderr)
                 if self.error_callback:
                     self.error_callback()
             finally:
@@ -73,7 +73,7 @@ class RemoteTaskList(TaskList):
             self.loading_callback()
 
         if self.settings.server_cert and not os.path.exists(self.settings.server_cert):
-            sys.stderr.write("Server certificate file not found")
+            print("Server certificate file not found", file=sys.stderr)
 
         message = Soup.Message.new('GET', self.url)
         soup_session.queue_message(message, self.download_finished_cb, None)

src/gtimelog/main.py
@@ -11,9 +11,9 @@ if DEBUG:
     def mark_time(what=None, _prev=[0, 0]):
         t = time.time()
         if what:
-            sys.stderr.write("{:.3f} ({:+.3f}) {}".format(t - _prev[1], t - _prev[0], what))
+            print("{:.3f} ({:+.3f}) {}".format(t - _prev[1], t - _prev[0], what), file=sys.stderr)
         else:
-            sys.stderr.write()
+            print(file=sys.stderr)
             _prev[1] = t
         _prev[0] = t
 else:
@@ -77,8 +77,8 @@ try:
     from gi.repository import Notify
     assert Notify.init("gtimelog")
 except ImportError:
-    sys.stderr.write("LibNotify (with introspection) not found.")
-    sys.stderr.write("Idle timeouts are not supported.")
+    print("LibNotify (with introspection) not found.", file=sys.stderr)
+    print("Idle timeouts are not supported.", file=sys.stderr)
 mark_time("Gtk imports done")
 
 from .allotropia import RemoteTaskList, soup_session  # noqa: E402
@@ -460,7 +460,7 @@ class MainWindow(object):
 
             self.main_window.move(x, y)
         except Exception as e:
-            sys.stderr.write(e.message)
+            print(e.message, file=sys.stderr)
 
     def save_ui_state(self, filename):
         try:
@@ -480,7 +480,7 @@ class MainWindow(object):
             config.write(uistaterc)
             uistaterc.close()
         except Exception as e:
-            sys.stderr.write(e.message)
+            print(e.message, file=sys.stderr)
 
     def set_up_log_view_columns(self):
         """Set up tab stops in the log view."""
@@ -588,7 +588,7 @@ class MainWindow(object):
         try:
             open(os.path.join(configdir, filename), 'w').write('%d' % number)
         except IOError as e:
-            sys.stderr.write('Unable to record ack...: ' + e.message)
+            print('Unable to record ack...: ' + e.message, file=sys.stderr)
             pass
 
     def ack_weekly_reminder(self, *args):
@@ -1249,8 +1249,8 @@ class MainWindow(object):
             with open(filename, 'rb') as f:
                 togglesdict = pickle.load(f)
         except (IOError, pickle.PickleError) as e:
-            sys.stderr.write("ERROR READING TOGGLE STATE FROM DISK")
-            sys.stderr.write(e)
+            print("ERROR READING TOGGLE STATE FROM DISK", file=sys.stderr)
+            print(e, file=sys.stderr)
             togglesdict = {}
 
         return togglesdict
@@ -1263,8 +1263,8 @@ class MainWindow(object):
             with open(filename, 'wb') as f:
                 pickle.dump(togglesdict, f)
         except (IOError, pickle.PickleError) as e:
-            sys.stderr.write("FAILED TO WRITE TOGGLE STATE TO DISK")
-            sys.stderr.write(e)
+            print("FAILED TO WRITE TOGGLE STATE TO DISK", file=sys.stderr)
+            print(e, file=sys.stderr)
 
     def on_row_expander_changed(self, treeview, iter, path, expanded):
         """Someone toggled a task list expander"""
@@ -1595,7 +1595,7 @@ class MainWindow(object):
                 self.welcome_back_notification.connect(
                     'closed', self.__notification_closed_cb)
         except Exception as e:
-            sys.stderr.write("pynotification failed: %s" % e)
+            print("pynotification failed: %s" % e, file=sys.stderr)
 
     def insert_old_log_entries(self, note=None, act=None, data=None):
         """
@@ -1867,7 +1867,7 @@ class SubmitWindow(object):
         soup_session.queue_message(message, self.upload_finished, automatic)
 
     def error_dialog(self, e, title='Error Communicating With The Server', automatic=False):
-        sys.stderr.write(e)
+        print(e, file=sys.stderr)
         if automatic:
             self.push_error_infobar(title, e)
         else:
@@ -2017,7 +2017,7 @@ class SubmitWindow(object):
                 continue
 
             if line and line != "Failed":
-                sys.stderr.write("Couldn't understand server: %s" % line)
+                print("Couldn't understand server: %s" % line, file=sys.stderr)
 
     def hide(self):
         self.window.hide()

src/gtimelog/secrets.py
@@ -35,7 +35,7 @@ class Authenticator(object):
             }
         )
     except ImportError:
-        sys.stderr.write("LibSecret not found. You will not be able to use the password keyring.")
+        print("LibSecret not found. You will not be able to use the password keyring.", file=sys.stderr)
         Secret = None
 
     def find_in_keyring(self, uri, callback):
@@ -71,7 +71,7 @@ class Authenticator(object):
                     self.SECRET_SCHEMA_COMPAT_NETWORK, attrs, None)
         except (GLib.Error, KeyError) as e:
             # Couldn't contact daemon, or other errors
-            sys.stderr.write("Unable to contact keyring: {0}".format(e))
+            print("Unable to contact keyring: {0}".format(e), file=sys.stderr)
 
         callback(username, password)
 
@@ -93,7 +93,7 @@ class Authenticator(object):
                 "Chronophage password for GTimelog", password, None)
         except GLib.Error as e:
             # Couldn't contact daemon, or other errors
-            sys.stderr.write("Unable to contact keyring: {0}".format(e))
+            print("Unable to contact keyring: {0}".format(e), file=sys.stderr)
 
     def ask_the_user(self, auth, uri, callback):
         """Pops up a username/password dialog for uri"""

src/gtimelog/timelog.py
@@ -253,7 +253,7 @@ class TimeWindow(object):
                     continue
 
                 if self.items and time <= self.items[-1][0]:
-                    sys.stderr.write("WARNING: This entry out of order: " + line)
+                    print("WARNING: This entry out of order: " + line, file=sys.stderr)
                 self.items.append((time, entry))
 
         # The entries really should be already sorted in the file
@@ -817,7 +817,7 @@ class TaskList(object):
                     if line and not line.startswith("#"):
                         self.items.add(line)
         except IOError as e:
-            sys.stderr.write(e.message)
+            print(e.message, file=sys.stderr)
             pass  # the file's not there, so what?
 
     def reload(self):